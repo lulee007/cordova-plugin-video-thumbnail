82288af5a1e5fbc4339dfa86d398d38e217c19c2
lulee007 <lulee007@live.com>
2015-10-22 15:51:08 +0800

    android init

---
+++ src/android/VideoThumbnail.java
@@ -0,0 +1,104 @@
+package com.lulee007.cordova.videothumbnail;
+
+import android.app.Activity;
+import android.graphics.Bitmap;
+import android.media.ThumbnailUtils;
+import android.provider.MediaStore;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Date;
+
+public class VideoThumbnail extends CordovaPlugin {
+
+    public boolean execute(String action, JSONArray args, final CallbackContext callbackContext)
+            throws JSONException {
+        Activity activity = this.cordova.getActivity();
+        if (action.equals("buildThumbnail")) {
+            final String videoPath = args.getString(0);
+            final int width = args.getInt(1)>0?args.getInt(1):100;
+            final int height = args.getInt(2)>0?args.getInt(2):100;
+            /**
+             * MediaStore.Video.Thumbnails.MICRO_KIND
+             */
+
+            final int kind = args.getInt(3)>0 && args.getInt(3)<4?args.getInt(3): MediaStore.Video.Thumbnails.MINI_KIND;
+            final String saveFolder = args.getString(4);
+
+            if(videoPath==null  || videoPath.isEmpty()){
+                callbackContext.error("videoPath was wrong");
+                return true;
+            }
+            if(saveFolder==null  || saveFolder.isEmpty()){
+                callbackContext.error("saveFolder was wrong");
+                return true;
+            }
+            cordova.getThreadPool().execute(new Runnable() {
+
+                public void run() {
+                    Bitmap bitmap = null;
+                    bitmap = getVideoThumbnail(videoPath, width, height, kind);
+                    if(bitmap==null){
+                        callbackContext.error("get video thumbnail failed,maybe videopath was wrong.");
+                        return;
+                    }
+
+                    FileOutputStream theOutputStream = null;
+                    try {
+                        Date now = new Date();
+                        String filePath = saveFolder.endsWith("/") ? saveFolder : saveFolder + "/thumbnail_" + now.getTime() + ".jpg";
+                        File theOutputFile = new File(filePath);
+                        if (!theOutputFile.exists()) {
+                            if (!theOutputFile.createNewFile()) {
+                                callbackContext.error("Could not save thumbnail.");
+                                return;
+                            }
+                        }
+                        if (theOutputFile.canWrite()) {
+                            theOutputStream = new FileOutputStream(theOutputFile);
+                            if (theOutputStream != null) {
+                                bitmap.compress(Bitmap.CompressFormat.JPEG, 75, theOutputStream);
+                            } else {
+                                callbackContext.error("Could not save thumbnail; target not writeable");
+                                return;
+                            }
+                            if (theOutputStream != null)
+                                theOutputStream.close();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        callbackContext.error("I/O exception saving thumbnail");
+                    } finally {
+                        if (bitmap != null && !bitmap.isRecycled()) {
+                            bitmap.recycle();
+                        }
+                    }
+                    callbackContext.success(); // Thread-safe.
+                }
+            });
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * @param videoPath 视频路径
+     * @param width
+     * @param height
+     * @param kind      eg:MediaStore.Video.Thumbnails.MICRO_KIND   MINI_KIND: 512 x 384，MICRO_KIND: 96 x 96
+     * @return
+     */
+    private Bitmap getVideoThumbnail(String videoPath, int width, int height,
+                                     int kind) {
+        // 获取视频的缩略图
+        Bitmap bitmap = ThumbnailUtils.createVideoThumbnail(videoPath, kind);
+        bitmap = ThumbnailUtils.extractThumbnail(bitmap, width, height, ThumbnailUtils.OPTIONS_RECYCLE_INPUT);
+        return bitmap;
+    }
+}
---
+++ www/videoThumbnail.js
@@ -0,0 +1,36 @@
+/**
+ * Created by luxiaohui on 15/10/22.
+ */
+/*
+ According to apache license
+
+ https://github.com/lulee007/cordova-plugin-video-thumbnail
+
+ */
+
+var exec = require('cordova/exec');
+
+var videoThumbnail = {
+
+    build: function(success, failure, config) {
+        exec(success || function() {},
+            failure || function() {},
+            'VideoThumbnail',
+            'buildThumbnail',
+            [
+                config.videoPath,
+                config.width,
+                config.height,
+                config.kind,
+                config.saveFolder
+            ]);
+    }
+
+
+};
+
+/* @Deprecated */
+window.plugins = window.plugins || {};
+window.plugins.videoThumbnail = videoThumbnail;
+
+module.exports = videoThumbnail;
