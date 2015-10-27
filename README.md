9b719614cf40f34fcb5776b8667770d09cf10390
lulee007 <lulee007@live.com>
2015-10-27 12:59:06 +0800

    fixed ios choose video file not found error

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
 # cordova-plugin-video-thumbnail
 获取本地视频的缩略图
+### Using the plugin
+`videoThumbnail.build`
+params:
+    * success callback function
+    * error callback function
+    * configs object
+```javascript
+window.plugins.videoThumbnail.build(
+      function (data) {
+        console.log(data);
+        $scope.thumbnail=data;
+        $scope.$apply();
+      }, function (error) {
+        console.log(error);
+      },
+      {
+        videoPath:"/Users/xxx/Library/Developer/CoreSimulator/Devices/7ECDF8F2-B5DC-4F10-BB1F-FF4FA3757BC1/data/Containers/Data/Application/BA3DD2ED-6EF5-4E41-943E-4232CC52D950/Documents/video1.mov",
+        width:100,
+        height:100,
+        kind:1//android only
+      });
+```
+### Installing the plugin
+```bash
+   cordova plugin add https://github.com/lulee007/cordova-plugin-video-thumbnail.git
+```
--- src/ios/VideoThumbnail.m
+++ src/ios/VideoThumbnail.m
@@ -1,81 +1,99 @@
 /********* VideoThumbnail.m Cordova Plugin Implementation *******/
 
 #import <Cordova/CDV.h>
-#import <AVFoundation/AVFoundation.h>
-#import <AVFoundation/AVAsset.h>
+    #import <AVFoundation/AVFoundation.h>
+    #import <AVFoundation/AVAsset.h>
 
-#import <MediaPlayer/MediaPlayer.h>
+
 @interface VideoThumbnail : CDVPlugin {
-    // Member variables go here.
+// Member variables go here.
 }
 
 - (void)buildThumbnail:(CDVInvokedUrlCommand*)command;
-@end
+    @end
 
-@implementation VideoThumbnail
+    @implementation VideoThumbnail
 
-- (void)buildThumbnail:(CDVInvokedUrlCommand*)command
+    - (void)buildThumbnail:(CDVInvokedUrlCommand*)command
 {
-    CDVPluginResult* pluginResult = nil;
-    __block NSString* payload = nil;
+  CDVPluginResult* pluginResult = nil;
+  __block NSString* payload = nil;
 
-    NSString* videoPath = [command.arguments objectAtIndex:0];
-    NSString* saveFolder =  NSTemporaryDirectory();
-    if(!videoPath ||videoPath.length==0){
-        payload=@"videoPath was wrong";
-        pluginResult=[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:payload];
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-        return;
-    }
-    
-    if(![saveFolder hasSuffix:@"/"]){
-        saveFolder=[NSString stringWithFormat:@"%@/",saveFolder];
-    }
-    NSTimeInterval  now=   [NSDate date].timeIntervalSince1970;
-    NSString* nowString=[NSString stringWithFormat:@"%.0f",now];
-    NSString * savePath=[NSString stringWithFormat:@"%@thumbnail_%@.jpg",saveFolder,nowString];
-    [self.commandDelegate runInBackground:^{
-        CDVPluginResult* pluginResult=nil;
-        if(extractVideoThumbnail(videoPath, savePath)){
-            payload=savePath;
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
-        }else{
-            payload=@"Could not save thumbnail";
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:payload];
-        }
-        
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    }];
-    
+  NSString* videoPath = [command.arguments objectAtIndex:0];
+  CGFloat width=[[command.arguments objectAtIndex:1] floatValue];
+width=width>0?width:100;
+CGFloat height=[[command.arguments objectAtIndex:2] floatValue];
+height=height>0?height:100;
+
+NSString* saveFolder =  NSTemporaryDirectory();
+if(!videoPath ||videoPath.length==0){
+  payload=@"videoPath was wrong";
+pluginResult=[CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:payload];
+[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+return;
 }
 
-BOOL extractVideoThumbnail ( NSString *theSourceVideoName,
-                            NSString *theTargetImageName )
+if(![saveFolder hasSuffix:@"/"]){
+  saveFolder=[NSString stringWithFormat:@"%@/",saveFolder];
+}
+NSTimeInterval  now=   [NSDate date].timeIntervalSince1970;
+NSString* nowString=[NSString stringWithFormat:@"%.0f",now];
+NSString * savePath=[NSString stringWithFormat:@"%@thumbnail_%@.jpg",saveFolder,nowString];
+[self.commandDelegate runInBackground:^{
+  CDVPluginResult* pluginResult=nil;
+  if(extractVideoThumbnail(videoPath, width,height,savePath)){
+  payload=savePath;
+  pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:payload];
+}else{
+  payload=@"Could not save thumbnail";
+pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:payload];
+}
+
+[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+}];
+
+}
+
+BOOL extractVideoThumbnail ( NSString *theSourceVideoName,CGFloat width,CGFloat height,
+NSString *theTargetImageName )
 {
-    
-    UIImage *thumbnail;
-    NSURL *url;
-    NSString *revisedTargetImageName = [[theTargetImageName stringByReplacingOccurrencesOfString:@"file://" withString:@""] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
-    
-    if ( [theSourceVideoName rangeOfString:@"://"].location == NSNotFound )
-    {
-        url = [NSURL URLWithString:[[@"file://localhost" stringByAppendingString:theSourceVideoName]
-                                    stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
-    }
-    else
-    {
-        url = [NSURL URLWithString:[theSourceVideoName stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding]];
-    }
-    
-    AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:[NSURL fileURLWithPath:theSourceVideoName] options:nil];
-    AVAssetImageGenerator *gen = [[AVAssetImageGenerator alloc] initWithAsset:asset];
-    gen.appliesPreferredTrackTransform = YES;
-    CMTime time = CMTimeMakeWithSeconds(0.0, 600);
-    NSError *error = nil;
-    CMTime actualTime;
-    CGImageRef image = [gen copyCGImageAtTime:time actualTime:&actualTime error:&error];
-    thumbnail = [[UIImage alloc] initWithCGImage:image];
-    CGImageRelease(image);
+  UIImage *thumbnail;
+  NSURL *url;
+  NSString *revisedTargetImageName = [[theTargetImageName stringByReplacingOccurrencesOfString:@"file://" withString:@""] stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+
+url=[NSURL fileURLWithPath:[theSourceVideoName stringByReplacingOccurrencesOfString:@"file://" withString:@""]];
+
+
+
+AVAsset* videoAsset = [AVAsset assetWithURL:url];
+
+AVAssetImageGenerator *imageGenerator = [[AVAssetImageGenerator alloc] initWithAsset:videoAsset];
+
+Float64 durationSeconds = CMTimeGetSeconds([videoAsset duration]);
+CMTime midpoint = CMTimeMakeWithSeconds(durationSeconds/2.0, 600);
+NSError* error = nil;
+CMTime actualTime;
+
+CGImageRef halfWayImage = [imageGenerator copyCGImageAtTime:midpoint actualTime:&actualTime error:&error];
+
+
+//       AVURLAsset *asset = [[AVURLAsset alloc] initWithURL:[NSURL fileURLWithPath:theSourceVideoName] options:nil];
+//    AVAssetImageGenerator *gen = [[AVAssetImageGenerator alloc] initWithAsset:asset];
+//    gen.appliesPreferredTrackTransform = YES;
+    //    gen.maximumSize = CGSizeMake(width , height);
+//
+//    CMTime time = CMTimeMakeWithSeconds(0.0, 600);
+//    NSError *error = nil;
+    //    CMTime actualTime;
+    //    CGImageRef image = [gen copyCGImageAtTime:time actualTime:&actualTime error:&error];
+if(!halfWayImage ||error){
+  NSLog(@"%@",error);
+return NO;
+}
+thumbnail = [[UIImage alloc] initWithCGImage:halfWayImage];
+CGImageRelease(halfWayImage);
+
+
     // write out the thumbnail; a return of NO will be a failure.
     return [UIImageJPEGRepresentation ( thumbnail, 1.0) writeToFile:revisedTargetImageName atomically:YES];
 }
